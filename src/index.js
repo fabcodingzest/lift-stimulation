d48553b1a5bf326341bf292e192dc512ab7311bd
Fab <fabrizvi786@gmail.com>
2022-09-19 19:36:39 +0000

    improved logic for speed of lift per floor

--- src/index.js
+++ src/index.js
@@ -52,13 +52,15 @@ function addClickToButtons() {
   });
 }
 
-function moveLift(floorId, freeLift) {
+function moveLift(id, freeLift) {
   if (!freeLift.classList.contains("busy")) {
-    const floorDur = floorId * 2;
-    freeLift.style.transition = `transform ${
-      floorDur !== 0 ? floorDur : 2
-    }s linear`;
-    freeLift.style.transform = `translateY(-${9.2 * parseInt(floorId)}rem)`;
+    let floorId = parseInt(id);
+    let currentFloor = parseInt(freeLift.dataset.floor) || 0;
+    freeLift.dataset.floor = floorId;
+    console.log(floorId);
+    const floorDur = Math.abs(currentFloor - floorId) * 2;
+    freeLift.style.transition = `transform ${floorDur}s linear`;
+    freeLift.style.transform = `translateY(-${9.2 * floorId}rem)`;
     freeLift.classList.add("busy");
     setTimeout(() => freeLift.classList.add("open"), (floorDur + 2) * 1000);
     setTimeout(
@@ -76,7 +78,7 @@ function handleMove(floorId) {
   setInterval(() => {
     if (floorOrder.length > 0) {
       freeLift = getFreeLift(allLiftArr);
-      if (!freeLift.classList.contains("busy")) {
+      if (!freeLift?.classList.contains("busy")) {
         moveLift(floorOrder.shift(), freeLift);
       } else {
         alert("No Free Lift atm");
@@ -88,7 +90,6 @@ function handleMove(floorId) {
 const getFreeLift = (allLift) => {
   const newLiftArr = Array.from(allLift);
   for (let i = 0; i < newLiftArr.length; i++) {
-    console.log(newLiftArr[i].classList);
     if (!newLiftArr[i].classList.contains("busy")) {
       return newLiftArr[i];
     }
