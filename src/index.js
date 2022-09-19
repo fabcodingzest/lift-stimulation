4cd615e57a0222a674d6ba22ba4522d393da48d4
Fab <fabrizvi786@gmail.com>
2022-09-19 16:53:59 +0000

    milestone 1 completed

--- src/index.js
+++ src/index.js
@@ -4,24 +4,32 @@ const noOfLifts = document.querySelector("#noOfLifts");
 const floorContainer = document.querySelector(".floor");
 const liftContainer = document.querySelector(".lift");
 
-create.addEventListener("click", (e) => {
+const floorOrder = [];
+
+create.addEventListener("click", () => {
   const floors = parseInt(noOfFloors.value, 10);
   const lifts = parseInt(noOfLifts.value, 10);
   if (floors > 0 && lifts > 0) {
-    //  now that we have input value, we should generate the UI for Floors and lift
-    // For that we need to loop through them to generate the number of lifts and floor acc to input
     let floorHTML = "";
     let liftHTML = "";
-    for (let i = 0; i < floors; i++) {
+    for (let i = floors - 1; i >= 0; i--) {
       floorHTML += `
         <div class="floor-box">
         <div class="floor-container">
           <div class="controllers">
-            ${i === floors - 1 ? "" : `<button class="up">Up</button>`}
-            ${i === 0 ? "" : `<button class="down">Down</button>`}
+            ${
+              i === floors - 1
+                ? ""
+                : `<button class="up" data-floor=${i}>Up</button>`
+            }
+            ${
+              i === 0
+                ? ""
+                : `<button class="down" data-floor=${i}>Down</button>`
+            }
           </div>
         </div>
-        <p class="floor-text">Floor 1</p>
+        <p class="floor-text">Floor ${i + 1}</p>
       </div>`;
     }
     for (let i = 0; i < lifts; i++) {
@@ -29,8 +37,52 @@ create.addEventListener("click", (e) => {
       <div class="lift-box"></div>
       `;
     }
-    console.log(floorHTML);
     floorContainer.innerHTML = floorHTML;
     liftContainer.innerHTML = liftHTML;
+
+    addClickToButtons();
   }
 });
+
+function addClickToButtons() {
+  const moveBtns = document.querySelectorAll(".controllers > button");
+  moveBtns.forEach((item) => {
+    const floorId = item.dataset.floor;
+    item.addEventListener("click", () => handleMove(floorId));
+  });
+}
+
+function moveLift(floorId, freeLift) {
+  if (!freeLift.classList.contains("busy")) {
+    freeLift.style.transform = `translateY(-${9.2 * parseInt(floorId)}rem)`;
+    freeLift.classList.add("busy");
+    setTimeout(() => freeLift.classList.remove("busy"), 3000);
+  }
+}
+
+function handleMove(floorId) {
+  const allLiftArr = document.querySelectorAll(".lift-box");
+  let freeLift = getFreeLift(allLiftArr);
+  floorOrder.push(floorId);
+  setInterval(() => {
+    if (floorOrder.length > 0) {
+      freeLift = getFreeLift(allLiftArr);
+      if (!freeLift.classList.contains("busy")) {
+        moveLift(floorOrder.shift(), freeLift);
+      } else {
+        alert("No Free Lift atm");
+      }
+    }
+  }, 2000);
+}
+
+const getFreeLift = (allLift) => {
+  const newLiftArr = Array.from(allLift);
+  for (let i = 0; i < newLiftArr.length; i++) {
+    console.log(newLiftArr[i].classList);
+    if (!newLiftArr[i].classList.contains("busy")) {
+      return newLiftArr[i];
+    }
+  }
+  return 0;
+};
