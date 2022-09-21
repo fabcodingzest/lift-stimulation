11c0f0c8e559754f02ba282c1be782f2a9d9535a
fabcodingzest <fabrizvi786@gmail.com>
2022-09-21 16:30:07 +0530

    added: validations and comments

--- src/index.js
+++ src/index.js
@@ -17,26 +17,35 @@ const reset = () => {
 create.addEventListener("click", () => {
   const floors = parseInt(noOfFloors.value, 10);
   const lifts = parseInt(noOfLifts.value, 10);
-  // Reset previous values and clear the UI
-  reset();
-  const halfNoOfFloors = Math.ceil((1 / 2) * floors);
-  if (!floors || !lifts) {
-    alert("Please enter both floor and lift numbers");
-  } else if (floors > 10 || lifts > floors || lifts > halfNoOfFloors) {
-    floors > 10 &&
-      alert("The app does not support more than 10 floors for now");
-    lifts > halfNoOfFloors &&
-      alert(
-        `${halfNoOfFloors} ${
-          halfNoOfFloors > 1 ? "lifts are" : "lift is"
-        } more than enough for ${floors} floors! ${lifts} would be unnecessary please re-enter the number :)`
-      );
-    lifts > floors && alert("Lifts cannot be more than the number of floors");
-  } else if (floors > 0 && lifts > 0) {
-    let floorHTML = "";
-    let liftHTML = "";
-    for (let i = floors - 1; i >= 0; i--) {
-      floorHTML += `
+  let numbers = /^[0-9]+$/;
+  if (
+    // if entered values are numbers, proceed
+    noOfFloors.value.match(numbers) &&
+    noOfLifts.value.match(numbers)
+  ) {
+    // Reset the form and UI if existed previously
+    reset();
+    const halfNoOfFloors = Math.ceil((1 / 2) * floors);
+    if (floors > 10 || lifts > floors || lifts > halfNoOfFloors) {
+      // Logical alerts for input values
+      // No more than 10 floors available
+      floors > 10
+        ? alert("The app does not support more than 10 floors for now")
+        : // Lifts can only be half the number of floors entered
+        lifts > halfNoOfFloors
+        ? alert(
+            `${halfNoOfFloors} ${
+              halfNoOfFloors > 1 ? "lifts are" : "lift is"
+            } more than enough for ${floors} floors! ${lifts} would be unnecessary please re-enter the number :)`
+          )
+        : // Lifts cannot be more than the number of floors
+          lifts > floors &&
+          alert("Lifts cannot be more than the number of floors");
+    } else if (floors > 0 && lifts > 0) {
+      let floorHTML = "";
+      let liftHTML = "";
+      for (let i = floors - 1; i >= 0; i--) {
+        floorHTML += `
         <div class="floor-box">
         <div class="floor-container">
           <div class="controllers">
@@ -54,21 +63,27 @@ create.addEventListener("click", () => {
         </div>
         <p class="floor-text">Floor ${i + 1}</p>
       </div>`;
-    }
-    for (let i = 0; i < lifts; i++) {
-      liftHTML += `
+      }
+      for (let i = 0; i < lifts; i++) {
+        liftHTML += `
       <div class="lift-box"></div>
       `;
-    }
-    floorContainer.innerHTML = floorHTML;
-    liftContainer.innerHTML = liftHTML;
+      }
+      floorContainer.innerHTML = floorHTML;
+      liftContainer.innerHTML = liftHTML;
 
-    addClickToButtons();
+      // adding Event listeners to all up and down buttons
+      addClickToButtons();
+    }
+  } else {
+    reset();
+    alert("Please fill both the fields with valid numbers that greater than 0");
   }
 });
 
 function addClickToButtons() {
   const moveBtns = document.querySelectorAll(".controllers > button");
+  // we gave data-floor to buttons while creating the UI hence can access them here for clicked floor ID
   moveBtns.forEach((item) => {
     const floorId = item.dataset.floor;
     item.addEventListener("click", () => handleMove(floorId));
@@ -78,20 +93,24 @@ function addClickToButtons() {
 function moveLift(id, freeLift) {
   if (!freeLift.classList.contains("busy")) {
     let floorId = parseInt(id);
+    // we set the data-set here so next time we can abstract it to get current floor
     let currentFloor = parseInt(freeLift.dataset.floor) || 0;
     freeLift.dataset.floor = floorId;
-    const floorDur = Math.abs(currentFloor - floorId) * 2;
-    freeLift.style.transition = `transform ${floorDur}s linear`;
+    const reachingTime = Math.abs(currentFloor - floorId) * 2;
+    freeLift.style.transition = `transform ${reachingTime}s linear`;
     freeLift.style.transform = `translateY(-${
-      (window.innerWidth > 650 ? 9 : 4) * floorId
+      (window.innerWidth > 650 ? 9 : 4) * floorId // for responsive height calculations
     }rem)`;
-    freeLift.classList.add("busy");
-    setTimeout(() => freeLift.classList.add("open"), (floorDur + 2) * 1000);
+    freeLift.classList.add("busy"); //marking it as busy
+    setTimeout(() => freeLift.classList.add("open"), (reachingTime + 2) * 1000); // opening door only after lift reaches the desired floor
     setTimeout(
       () => freeLift.classList.remove("open"),
-      (floorDur + 4.5) * 1000
+      (reachingTime + 4.5) * 1000
     );
-    setTimeout(() => freeLift.classList.remove("busy"), (floorDur + 7) * 1000);
+    setTimeout(
+      () => freeLift.classList.remove("busy"),
+      (reachingTime + 7) * 1000
+    );
   }
 }
 
@@ -99,20 +118,24 @@ function handleMove(floorId) {
   const allLiftArr = document.querySelectorAll(".lift-box");
   let freeLift = getFreeLift(allLiftArr);
   floorOrder.push(floorId);
+  // setting Interval after every second to check for any floors that are in order
   setInterval(() => {
     if (floorOrder.length > 0) {
+      // getting freeLift available
       freeLift = getFreeLift(allLiftArr);
       if (!freeLift?.classList.contains("busy")) {
-        moveLift(floorOrder.shift(), freeLift);
+        moveLift(floorOrder.shift(), freeLift); // we pass the first item in array as we push floors to the back of array
       } else {
-        alert("No Free Lift atm");
+        console.log("No Free Lift atm");
       }
     }
   }, 1000);
 }
 
 const getFreeLift = (allLift) => {
+  // Converting NodeList to array
   const newLiftArr = Array.from(allLift);
+  // Returning whichever lift is free at the moment
   for (let i = 0; i < newLiftArr.length; i++) {
     if (!newLiftArr[i].classList.contains("busy")) {
       return newLiftArr[i];
